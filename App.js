4d068030240555fd8fda5565397c9cd710978912
farrukhehsan90 <farrukhehsan90@gmail.com>
2021-09-09 02:46:50 +0500

    update code

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Game from './src/game';
+
+export default function App() {
+  return (
+ 
+    <Game></Game>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
