36199ade6d81c9d07ebe279be9b00e9026715b0e
Farrukh Ehsan <farrukhehsan90@gmail.com>
2021-09-13 01:40:00 +0500

    Make Component folder and add game and card file there and make cofig folder and make there json file and import in checkJsonText and fetch data from json
    Make Component folder and add game and card file there and make cofig folder and make there json file and import in checkJsonText and fetch data from json

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Game from './src/game';
+import Game from './src/component/game';
+import CheckjsonText from './src/component/CheckJsonText';
 
 export default function App() {
   return (
-
-    <Game />
+    // <Game />
+    <CheckjsonText />
   );
 }
 
